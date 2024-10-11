8db47321563565ae49c024a14b76d244c00559ff
milkymilk <fluffypuffmalloween@gmail.com>
2024-10-11 13:58:55 -0600

    moved started code to respository

---
+++ server/src/service/weatherService.ts
@@ -0,0 +1,65 @@
+import { LargeNumberLike } from 'crypto';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  lat: number;
+  lng: number;
+}
+
+// TODO: Define a class for the Weather object
+class Weather {
+  city: string;
+  date: string;
+  icon: string;
+  tempF: number;
+  windSpeed: number;
+  humidity: number;
+  iconDescription: string;
+
+  constructor(city: string, date: string,icon: string,  tempF: number, windSpeed: number, humidity: number,iconDescription: string ) {
+    this.city = city;
+    this.date = date;
+    this.icon = icon;
+    this.tempF = tempF;
+    this.windSpeed = windSpeed;
+    this.humidity = humidity;
+    this.iconDescription = iconDescription;
+  }
+
+}
+
+// TODO: Complete the WeatherService class
+class WeatherService {
+  // TODO: Define the baseURL, API key, and city name properties
+  private baseURL: string;
+  private apiKey: string;
+  private city: string;
+
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL || '';
+    this.apiKey = process.env.API_KEY || '';
+    this.city = '';
+  }
+  // TODO: Create fetchLocationData method
+    private async fetchLocationData(query: string) {}
+  // TODO: Create destructureLocationData method
+  // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  // TODO: Create buildGeocodeQuery method
+  // private buildGeocodeQuery(): string {}
+  // TODO: Create buildWeatherQuery method
+  // private buildWeatherQuery(coordinates: Coordinates): string {}
+  // TODO: Create fetchAndDestructureLocationData method
+  // private async fetchAndDestructureLocationData() {}
+  // TODO: Create fetchWeatherData method
+  // private async fetchWeatherData(coordinates: Coordinates) {}
+  // TODO: Build parseCurrentWeather method
+  // private parseCurrentWeather(response: any) {}
+  // TODO: Complete buildForecastArray method
+  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  // TODO: Complete getWeatherForCity method
+  // async getWeatherForCity(city: string) {}
+}
+
+export default new WeatherService();
