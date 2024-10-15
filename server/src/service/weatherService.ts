318a5ee6c93620842c7747bd6d96344d0c91b88b
milkymilk <fluffypuffmalloween@gmail.com>
2024-10-14 18:00:43 -0600

    added work done to weatherservice.ts

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -4,8 +4,12 @@ dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
 interface Coordinates {
+  name: string;
   lat: number;
-  lng: number;
+  lon: number;
+  country: string;
+  state: string;
+
 }
 
 // TODO: Define a class for the Weather object
@@ -18,7 +22,7 @@ class Weather {
   humidity: number;
   iconDescription: string;
 
-  constructor(city: string, date: string,icon: string,  tempF: number, windSpeed: number, humidity: number,iconDescription: string ) {
+  constructor(city: string, date: string,icon: string,  tempF: number, windSpeed: number, humidity: number, iconDescription: string ) {
     this.city = city;
     this.date = date;
     this.icon = icon;
@@ -43,23 +47,78 @@ class WeatherService {
     this.city = '';
   }
   // TODO: Create fetchLocationData method
-    private async fetchLocationData(query: string) {}
+    private async fetchLocationData(query: string) {
+      const response = await fetch(query)
+      return response
+      
+    }
   // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  private destructureLocationData(locationData: Coordinates): Coordinates {
+    const coordinates: Coordinates = {
+      name: locationData.name,
+      lat: locationData.lat,
+      lon: locationData.lon,
+      country: locationData.country,
+      state: locationData.state
+
+    }
+    return coordinates;
+    
+  }
   // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
+  private buildGeocodeQuery(): string {
+    const query = `${this.baseURL}/geo/1.0/direct?q=${this.city}&limit=1&appid=${this.apiKey}`
+    return query;
+  }
   // TODO: Create buildWeatherQuery method
-  // private buildWeatherQuery(coordinates: Coordinates): string {}
+  private buildWeatherQuery(coordinates: Coordinates): string {
+    const locationData: Coordinates = {
+      name: coordinates.name,
+      lat: coordinates.lat,
+      lon: coordinates.lon,
+      country: coordinates.country,
+      state: coordinates.state
+    }
+    const query = `${this.baseURL}/data/2.5/forecast?lat=${locationData.lat}&lon=${locationData.lon}&appid=${this.apiKey}`
+    return query;
+  }
   // TODO: Create fetchAndDestructureLocationData method
-  // private async fetchAndDestructureLocationData() {}
+  private async fetchAndDestructureLocationData() {
+    const query = this.buildGeocodeQuery()
+    const locationData = await this.fetchLocationData(query)
+    const coordinates = this.destructureLocationData(locationData)
+
+
+  }
   // TODO: Create fetchWeatherData method
-  // private async fetchWeatherData(coordinates: Coordinates) {}
+  private async fetchWeatherData(coordinates: Coordinates) {
+    const response = await fetch (this.buildWeatherQuery(coordinates))
+    return response;
+  }
   // TODO: Build parseCurrentWeather method
-  // private parseCurrentWeather(response: any) {}
+  private parseCurrentWeather(response: any) {
+    const data = response.json()
+    return data;
+
+  }
   // TODO: Complete buildForecastArray method
-  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
+    const weather: Weather = {
+    city: currentWeather.city,
+    date: currentWeather.date,
+    icon: currentWeather.icon,
+    tempF: currentWeather.tempF,
+    windSpeed: currentWeather.windSpeed,
+    humidity: currentWeather.humidity,
+    iconDescription: currentWeather.iconDescription
+
+    }
+
+  }
   // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+  async getWeatherForCity(city: string) {
+    
+  }
 }
 
 export default new WeatherService();
